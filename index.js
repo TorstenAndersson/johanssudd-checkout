bb8783a0e4b50ce8e97a52531de944deb8099108
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-05-23 14:58:44 +0200

    back to redirect

--- index.js
+++ index.js
@@ -8,26 +8,37 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 app.post("/create-checkout-session", async (req, res) => {
-  const session = await stripe.checkout.sessions.create({
-    payment_method_types: ["card"],
-    line_items: [
-      {
-        price_data: {
-          currency: "usd",
-          product_data: {
-            name: "T-shirt",
-          },
-          unit_amount: 2000,
-        },
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: "https://example.com/success",
-    cancel_url: "https://example.com/cancel",
-  });
+    /*
+    const { items } = req.body;
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: 199.99,//calculateOrderAmount(items),
+        currency: "sek"
+    });
 
-  res.json({ id: session.id });
+    res.send({
+        clientSecret: paymentIntent.client_secret
+    });
+    */
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+            {
+                price_data: {
+                currency: "usd",
+                product_data: {
+                name: "T-shirt",
+                },
+                unit_amount: 2000,
+                },
+                quantity: 1,
+            },
+        ],
+        mode: "payment",
+        success_url: "https://example.com/success",
+        cancel_url: "https://example.com/cancel",
+    });
+
+    res.json({ id: session.id });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${ PORT }!`));

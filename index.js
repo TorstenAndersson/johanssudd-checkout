8c9a0393fd521ff358c98515209d51792e952202
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-05-28 17:27:44 +0200

    testing

--- index.js
+++ index.js
@@ -61,9 +61,9 @@ app.post("/swish", async (req, res) => {
     });
 
     const agent = new https.Agent({
-        cert: fs.readFileSync('ssl/Swish_Merchant_TestCertificate_1234679304.pem', { encoding: 'utf8' }),
-        key: fs.readFileSync('ssl/Swish_Merchant_TestCertificate_1234679304.key', { encoding: 'utf8' }),
-        ca: fs.readFileSync('ssl/Swish_TLS_RootCA.pem', { encoding: 'utf8' }),
+        cert: fs.readFileSync('ssl/Swish_Merchant_TestCertificate_1234679304.pem'),
+        key: fs.readFileSync('ssl/Swish_Merchant_TestCertificate_1234679304.key'),
+        ca: fs.readFileSync('ssl/Swish_TLS_RootCA.pem'),
     });
 
     console.log("inte certifikat när de skapades hallå");

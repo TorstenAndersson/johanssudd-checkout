e99580191b35278bc5d70862c383cf4ed85c2c72
Mikael de Verdier <mikael.deverdier@gmail.com>
2021-05-29 18:09:39 +0200

    testing

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const stripe = require("stripe")("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");
 //const path = require("path")
-const https = require("https");
+const request = require("request");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 const fs = require('fs');
@@ -60,6 +60,34 @@ app.post("/swish", async (req, res) => {
         message: "Johans Tisha"
     });
 
+    request({
+        method: "POST",
+        uri: "mss.cpc.getswish.net/swish-cpcapi/api/v2/paymentrequests/" + uuid,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            payeePaymentReference: "0123456789",
+            callbackUrl: "https://johanssudd-checkout.herokuapp.com/callback",
+            payeeAlias: "1231181189",
+            currency: "SEK",
+            payerAlias: "4671234768",
+            amount: "100",
+            message: "Johans Tisha"
+        }),
+        agentOptions: {
+            cert: fs.readFileSync('./ssl/Swish_Merchant_TestCertificate_1234679304.pem'),
+            key: fs.readFileSync('./ssl/Swish_Merchant_TestCertificate_1234679304.key'),
+            ca: fs.readFileSync('./ssl/Swish_TLS_RootCA.pem')
+        }
+      }, (error, httpResponse, body) => {
+        console.log(error)
+        console.log(httpResponse)
+        console.log(body)
+      });
+
+    /*
+
     const agent = new https.Agent({
         cert: fs.readFileSync('./ssl/Swish_Merchant_TestCertificate_1234679304.pem'),
         key: fs.readFileSync('./ssl/Swish_Merchant_TestCertificate_1234679304.key'),
@@ -98,6 +126,7 @@ app.post("/swish", async (req, res) => {
       
     request.write(data);
     request.end();
+    */
 
       /*
 
